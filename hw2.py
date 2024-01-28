002b3fef6ee25e775e3c872dcff7abc0fb3f70a8
samkelson <samkelson@brandeis.edu>
2024-01-28 12:40:51 -0500

    init commit

---
+++ hw2.py
@@ -0,0 +1,66 @@
+def is_in_first_quadrant(vector: list) -> bool:
+  '''
+  Parameters
+  ----------
+  - vector: length 2 list
+
+  Returns
+  -------
+  Boolean
+
+
+
+  Returns True when vector is in the first quadrant (ie, 
+  all of its entries are non-negative). Returns 
+  False otherwise.
+  '''
+  # write your code here
+  pass
+
+
+def dot(v1: list, v2: list) -> float:
+  '''
+  Parameters
+  ----------
+  - v1: list
+  - v2: list
+
+  Returns
+  -------
+  float
+
+  Returns the dot product of v1 and v2
+  '''
+  pass
+
+
+def norm(v: list) -> float:
+  '''
+  Parameters
+  ----------
+  - v: list
+
+  Returns
+  -------
+  float
+
+  Returns the Euclidean norm of v
+  '''
+  pass
+
+
+def largest_norm(vectors: list[list]) -> list:
+  '''
+  Parameters
+  ----------
+  - vectors: list of lists
+
+  Returns
+  -------
+  - v: vector
+
+  Returns the vector v which has the largest norm 
+  of all the vectors in 'vectors'. In case of a tie, returns 
+  the first vector in the list.
+  '''
+  pass
